eb54edd2b4e472b63b8e4bf4144e2f28bbcfd3b3
ryandewsbury <ryan.dewsbury@kooltra.com>
2022-01-28 17:42:16 -0500

    fix width

--- bird.js
+++ bird.js
@@ -3,7 +3,7 @@ const ctx = cvs.getContext("2d");
 
 function draw() {
     ctx.fillStyle = "cyan";
-    ctx.fillRect(0, 0, world.w, world.h);
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
 
     requestAnimationFrame(draw);
 }
